74372a673eb5ef27623a24bc845aa73afdcdfe18
Prajwal S N <prajwal@deepsource.io>
2022-11-10 15:56:41 +0530

    test: 75
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 #[allow(dead_code)]
 use std::sync::atomic::AtomicUsize;
 
+#[repr(usize)]
+enum Test {
+    X = 0x1_000_000_000,
+    Y,
+}
+
 fn main() {
     while 100 > 20 {
         const ATOM: AtomicUsize = AtomicUsize::new(0);
         println!("{:?}", ATOM);
-    }
-
-    while true {
-        println!("potato");
-    }
-
-    while 20 < 5 {
-        println!("mega potato");
     }
 }
