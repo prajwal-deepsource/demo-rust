951dc761c16e9270da3a4b4de058b25d6ab9c666
Prajwal S N <prajwal@deepsource.io>
2022-11-29 19:47:53 +0530

    test: 1106
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
 #[allow(dead_code)]
-use std::sync::atomic::AtomicUsize;
-
-enum EnumABC {
-    A(i32),
-    B(u32),
-    C(&'static str),
-}
-
-fn bar() -> EnumABC {
-    EnumABC::B(10u32)
-}
-
-fn foo() {
-    let EnumABC::A(_) = bar() else { return };
-}
-
-#[repr(usize)]
-enum Test {
-    X = 0x1_000_000_000,
-    Y,
-}
+use std::rc::Rc;
 
 fn main() {
-    while 100 > 20 {
-        const ATOM: AtomicUsize = AtomicUsize::new(0);
-        println!("{:?}", ATOM);
-    }
+    let mut v = Vec::new();
+    let r = Rc::new(10);
+    v.push(r.clone());
 }
