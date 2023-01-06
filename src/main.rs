305f1da4967a76881ee37e048d556d8ba7245335
Prajwal S N <prajwal@deepsource.io>
2023-01-06 15:11:13 +0530

    test: 1022 with docs
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ struct A {
     b: &'static str,
 }
 
+// This is an impl item for the A type
 impl A {
+    // This is a scammy default
     fn default() -> A {
         A { a: 0, b: "Hola" }
     }
