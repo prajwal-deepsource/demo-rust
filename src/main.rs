23f426654ef9fbb2c41160e1b9e80e29cbbd266a
Prajwal S N <prajwal@deepsource.io>
2023-01-12 14:31:41 +0530

    test: S-1009
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use unknown_crate;
+use actix_web_lab::web as web_lab;
 
 fn main() {
-    let port = unknown_crate::Port();
-    drop(port);
+    let a: web_lab::Redirect = get_redir("/yello".into());
+    let b: web_lab::Redirect = get_direct_redir("/yello".into());
+}
+
+fn get_direct_redir(dest: &str) -> web_lab::Redirect {
+    web_lab::Redirect = web_lab::redirect("/".into(), dest.into())
+}
+
+fn get_redir(dest: &str) -> web_lab::Redirect {
+    web_lab::Redirect::new("/".into(), dest.into())
+}
+
+fn get_sanitised_redir(dest: &str) -> web_lab::Redirect {
+    let san = sanitise(dest);
+    web_lab::Redirect::new("/".into(), dest.into())
+}
+
+fn sanitise(s: &str) -> &str {
+    s
 }
