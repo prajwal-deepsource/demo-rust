28473ae7b4a9baa6904ab7d38e27b396349a5c4f
Prajwal S N <prajwal@deepsource.io>
2023-01-12 14:31:41 +0530

    test: S-1009
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-use unknown_crate;
+use actix_web_lab::web as web_lab;
 
-fn main() {
-    let port = unknown_crate::Port();
-    drop(port);
+fn redir_fn(dest: String, other_dest: String) -> web_lab::Redirect {
+    web_lab::redirect("Hello".into(), dest)
+}
+
+fn redir_struct_method(dest: &str) -> web_lab::Redirect {
+    web_lab::Redirect::new("Hello".into(), dest.into())
+}
+
+fn redir_with_cast(dest: &str) -> web_lab::Redirect {
+    web_lab::redirect("Hello".into(), dest as String)
+}
+
+fn no_match_on_field(destTuple: (String, usize)) -> web_lab::Redirect {
+    // Should not raise on this call
+    web_lab::redirect("Hello".into(), dest.0)
+}
+
+fn no_match_used_var(dest: String) -> Option<web_lab::Redirect> {
+    if !valid(dest) {
+        return None;
+    };
+    // Should not raise on this call
+    Some(web_lab::redirect("Hello".into(), dest))
 }
