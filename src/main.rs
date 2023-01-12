069e6b1094746ff9ef6bef7da1d77fcb986691ef
Prajwal S N <prajwal@deepsource.io>
2023-01-12 13:43:02 +0530

    test: 1021 FP
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-struct A {
-    a: i32,
-    b: &'static str,
-}
+use unknown_crate;
 
-// This is an impl item for the A type
-impl A {
-    // This is a scammy default
-    fn default() -> A {
-        A { a: 0, b: "Hola" }
-    }
-    fn print_a(&self) {
-        println!("{}", self.a);
-    }
-    fn print_b(&self) {
-        println!("{}", self.b);
-    }
+fn main() {
+    let port = unknown_crate::Port();
+    drop(port);
 }
-
-fn main() {}
