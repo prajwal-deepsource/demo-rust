679f93fa3e7e6c94159bb11dc58018e066602f72
Prajwal S N <prajwal@deepsource.io>
2023-01-19 13:27:17 +0530

    test: bunch of autofixes
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,13 @@
-use actix_web_lab::web as web_lab;
-use regex;
+#[forbid(dead_code)]
+use foo::bar;
 
-fn redir_fn(dest: String, other_dest: String) -> web_lab::Redirect {
-    web_lab::redirect("Hello", dest)
+fn main() {
+    print!("Hello there\n");
+    'x' as u8;
+    ["A", "B"].iter().map(|x| x.clone());
+    "Hello".splitn(2, 'l');
+    let a: Rc<String> = Rc::new("hello".to_string());
+    let b: Rc<&str> = Rc::new("hello");
+    t.seek(SeekFrom::Start(0));
+    t.seek(SeekFrom::Current(-t.seek_position()));
 }
-
-fn redir_struct_method(dest: &str) -> web_lab::Redirect {
-    web_lab::Redirect::new("Hello".into(), dest.into())
-}
-
-fn writer_check<W: std::io::Write>(w: &mut W) -> std::io::Result<()> {
-    w.write(b"foo")?;
-    Ok(())
-}
-
-fn random(path: String) {
-    let x = 0 as *const u32;
-    let y = 0 as *mut *const u32;
-    let z = 0 as *mut u32;
-    actix_files::NamedFile::open(path).unwrap();
-    let a: f32 = 0.0;
-    if a == f32::NAN {}
-    if a != f32::NAN {}
-}
-
-struct A;
-impl A {
-    fn default() -> Self {
-        Self
-    }
-}
-
-fn main() {}
