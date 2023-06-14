767b6951408e220e97856e8531e35fcd47b5f435
Prajwal S N <prajwal@deepsource.io>
2023-06-14 11:08:03 +0530

    test: FP
    Signed-off-by: Prajwal S N <prajwal@deepsource.io>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {}
-
-pub fn foo() {}
+fn main() {
+    let mut v = vec![1, 2, 3, 4];
+    v = v.into_iter().filter(|&e| e != 2).collect();
+    let v2: Vec<i32> = v.into_iter().filter(|&e| e != 2).collect();
+}
